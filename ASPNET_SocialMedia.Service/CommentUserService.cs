aabdfe6f544274d48566caca52243ee65796a828
Hakim Male <malehakim@gmail.com>
2016-04-08 22:13:32 +0200

    Added Web.Core

---
+++ ASPNET_SocialMedia.Service/CommentUserService.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using ASPNET_SocialMedia.Data.Infrastructure;
+using ASPNET_SocialMedia.Model.Models;
+using ASPNET_SocialMedia.Data.Repository;
+using ASPNET_SocialMedia.Core.Common;
+using ASPNET_SocialMedia.Service.Properties;
+using System;
+
+namespace ASPNET_SocialMedia.Service
+{
+
+    public interface ICommentUserService
+    {
+
+        IEnumerable<int> GetCommentIdsByUser(string userId);
+        ApplicationUser GetUser(int commentId);
+        void CreateCommentUser(string userId, int commentId);
+        void DeleteCommentUser(string userId, int commentId);
+
+        void DeleteCommentUser(int id);
+        void SaveCommentUser();
+       
+    }
+
+    public class CommentUserService : ICommentUserService
+    {
+        private readonly ICommentUserRepository commentUserRepository;
+        private readonly IUserRepository userRepository;
+        private readonly IUnitOfWork unitOfWork;
+
+        public CommentUserService(ICommentUserRepository commentUserRepository,IUserRepository userRepository,IUnitOfWork unitOfWork)
+        {
+            this.commentUserRepository = commentUserRepository;
+            this.userRepository = userRepository;
+            this.unitOfWork = unitOfWork;
+
+        }
+       
+        #region ICommentUserService Members
+
+        public ApplicationUser GetUser(int commentId)
+        {
+            var userId = commentUserRepository.Get(cu => cu.CommentId == commentId).UserId;
+            return userRepository.GetById(userId);
+        }
+
+        public IEnumerable<int> GetCommentIdsByUser(string userId)
+        {
+            List<int> commentIds = new List<int> { };
+            var commentUsers = commentUserRepository.GetMany(c => c.UserId == userId);
+            foreach (var item in commentUsers)
+            {
+                commentIds.Add(item.CommentId);
+            }
+            return commentIds;
+
+        }
+
+        public void DeleteCommentUser(string userId, int commentId)
+        {
+            var commentUser = commentUserRepository.Get(cu => cu.UserId == userId && cu.CommentId == commentId);
+            commentUserRepository.Delete(commentUser);
+            SaveCommentUser();
+        }
+
+        public void DeleteCommentUser(int id)
+        {
+            var commentUser = commentUserRepository.GetById(id);
+            commentUserRepository.Delete(commentUser);
+            SaveCommentUser();
+        }
+
+       
+        public void SaveCommentUser()
+        {
+            unitOfWork.Commit();
+        }
+
+        #endregion
+
+
+        public void CreateCommentUser(string userId, int commentId)
+        {
+            var commentUser = new CommentUser { UserId = userId, CommentId = commentId };
+            commentUserRepository.Add(commentUser);
+            SaveCommentUser();
+        }
+    }
+}
---
+++ ASPNET_SocialMedia.Service/Enums.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel;
+
+namespace ASPNET_SocialMedia.Service
+{
+    public enum GroupFilter
+    {
+        [Description("All")]
+        All,
+
+        [Description("My Groups")]
+        MyGroups,
+
+        [Description("My Followings Groups")]
+        MyFollowingsGroups,
+
+        [Description("My Followed Groups")]
+        MyFollowedGroups
+    }
+}
