64e846858f0e2c7fe8ef607437ddd228cbf0acb9
Hakim Male <malehakim@gmail.com>
2016-04-08 21:04:08 +0200

    Initial Commit

---
+++ ASPNET_SocialMedia.Web.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ASPNET_SocialMedia.Web.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ ASPNET_SocialMedia.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ASPNET_SocialMedia.Web.MvcApplication" Language="C#" %>
