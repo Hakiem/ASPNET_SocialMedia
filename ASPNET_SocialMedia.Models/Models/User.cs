aed30c927faf8a48daeafd48a6ab0c63c5c43e6d
Hakim Male <malehakim@gmail.com>
2016-04-08 21:33:58 +0200

    Remove all un-necessary namespaces in the models classes

--- ASPNET_SocialMedia.Models/Models/User.cs
+++ ASPNET_SocialMedia.Models/Models/User.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Text;
-using ASPNET_SocialMedia.Core.Common;
+﻿
 
 namespace ASPNET_SocialMedia.Model.Models
 {
